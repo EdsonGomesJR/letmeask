bb36f9cc95689299bd4abe21d6eaa03b79669dd2
EdsonGomesJr <edyz86@hotmail.com>
2021-06-22 11:02:56 -0300

    Aula 2 - Finalizada

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import {Route, BrowserRouter} from 'react-router-dom';
+import {AuthContextProvider} from './contexts/AuthContext'
+
+ function App() {
+    return (
+     <AuthContextProvider>
+        <BrowserRouter>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+        </BrowserRouter>
+      </AuthContextProvider>
+  
+
+  );
+}
+
+export default App;
